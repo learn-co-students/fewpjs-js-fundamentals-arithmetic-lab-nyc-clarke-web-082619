18427b791f8c73dce7091e170f03f9a3b2cb7da5
JazzyMussels <jeffmussels@gmail.com>
2019-10-04 11:30:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+var num1 = 2
+var num2 = 31
+multiply = num1 * num2
+multiply
+
+var random = Math.random()
+random 
+
+var mod = 12 % 8
+mod 
+
+var max = Math.max(5, 20)
+max 
